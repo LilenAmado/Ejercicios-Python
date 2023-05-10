22f9fccf24399325806b7bb4fae0b564e305168b
LilenAmado <65418208+LilenAmado@users.noreply.github.com>
2023-05-09 22:08:06 -0300

    Update Resumen_SQL.sql

--- Resumen_SQL.sql
+++ Resumen_SQL.sql
@@ -1,12 +1,56 @@
 -- Esquema, una división lógica de la base de datos para organizar los objetos. 
 
 -- Select 
-    -- Hacer una consulta de todos los datos y columnas de la tabla
-    SELECT * -- no es la mejor práctica porque traes todo
+    -- Muestra toda la tabla
+    SELECT * -- No es la mejor práctica porque traes todo
     FROM customers
 
     -- Columnas especificas al mostrar
-    SELECT companyname as Compañia, country as País
+    SELECT 
+    companyname AS Compania
+    , country AS Pais
+    , [Address] AS Direccion
     FROM customers
 
+    -- Cálculo en la consulta
+    SELECT 
+    ProductName
+    , UnitPrice
+    , UnitPrice * 12 as ImpuestoIVA
+    FROM Products
 
+    SELECT
+    OrderID
+    , ProductID
+    , UnitePrice
+    , Quantity
+    , Discount
+    , (UnitePrice * Quantity) - (UnitePrice * Quantity * Discount) as Parcial
+    FROM [Order Details]
+
+    -- Función Año en consulta
+    SELECT
+    CustomerID
+    , OrderID  
+    , OrderDate
+    , YEAR(OrderDate) as Anio
+    FROM [Order Details]
+
+    -- Distinct: Para sacar duplicados
+    SELECT DISTINCT 
+    Country
+    FROM Customers
+    Order by Country
+
+    -- Función Case
+    SELECT 
+    ProductName
+    , Category = 
+        CASE CategoryID
+            WHEN 1 THEN 'Bebidas'
+            WHEN 2 THEN 'Lacteos'
+            WHEN 3 THEN 'OTros'
+            ELSE 'No se encuentra en venta'
+        END -- AS Category
+    FROM Products
+    ORDER BY ProductName
