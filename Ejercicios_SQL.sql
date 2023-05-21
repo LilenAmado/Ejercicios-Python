0198d50e835fdf62c622e9ed176672daa7a0ebaa
Lilén Amado <lilen.amado.estudio@gmail.com>
2023-05-21 18:32:37 -0300

    SQL

--- Ejercicios_SQL.sql
+++ Ejercicios_SQL.sql
@@ -22,3 +22,64 @@ rating
 , count(title) as "Num movies"
 FROM Film
 Group by rating
+
+-- Challenge:
+-- A customer forgot their wallet at our store! We need to track down their email to inform them.
+-- What is the email for the customer with the name Nancy Thomas? 
+
+SELECT 
+	first_name, 
+	last_name, 
+	email
+FROM customer
+WHERE first_name = 'Nancy' and last_name = 'Thomas'
+
+-- Challenge:
+-- A customer wants to know what the movie "Outlaw Hanky" is about. 
+-- Could you give them the description for the mvoie "Outlaw Hanky"?
+
+SELECT 
+	film_id
+	, title 
+	, description
+FROM film
+WHERE title = 'Outlaw Hanky'
+
+-- Challenge:
+-- A customer is late on their movie return, and we've mailed them a letter to their address at "259 Ipoh Drive". We should also call them on the phone to let them know.
+-- Can you get the phone number for the customer who lives at "259 Ipoh Drive"?
+
+SELECT 
+	C.first_name
+	, C.last_name
+	, A.address
+	, A.phone
+FROM customer as C
+INNER JOIN address as A
+ON C.address_id = A.address_id
+WHERE A.address = '259 Ipoh Drive'
+
+-- Challenge:
+-- We want to reward out first 10 paying customers.
+-- What are the customer ids of the first 10 customers who created a payment?
+
+SELECT customer_id, amount, payment_date
+FROM payment
+ORDER BY payment_date asc
+LIMIT 10
+
+-- Challenge:
+-- A customer wants to quickly rent a video to watch over their short lunch break. 
+-- What are the titles of the 5 shortest (in length of runtime) movie? 
+
+SELECT title
+FROM FILM
+ORDER BY length asc
+LIMIT 5
+
+-- Challenge:
+-- If the previous customer can watch any movie that is 50 minutes or less in run time, how many options does she have? 
+
+SELECT count(title) as Num_movies
+FROM film
+WHERE length <= 50
--- Resumen_SQL.sql
+++ Resumen_SQL.sql
@@ -1,61 +1,71 @@
 -- Esquema, una división lógica de la base de datos para organizar los objetos. 
+"""
+=
+<
+>
+>=
+<=
+<> or !=
 
+AND - OR - NOT
+
+"""
 -- Select 
     -- Muestra toda la tabla
     SELECT * -- No es la mejor práctica porque traes todo
     FROM customers
 
-    -- Columnas especificas al mostrar
+-- Columnas especificas al mostrar
     SELECT 
-    companyname AS Compania
-    , country AS Pais
-    , [Address] AS Direccion
+        companyname AS Compania
+        , country AS Pais
+        , [Address] AS Direccion
     FROM customers
 
-    -- Cálculo en la consulta
+-- Cálculo en la consulta
     SELECT 
-    ProductName
-    , UnitPrice
-    , UnitPrice * 12 as ImpuestoIVA
+        ProductName
+        , UnitPrice
+        , UnitPrice * 12 as ImpuestoIVA
     FROM Products
 
     SELECT
-    OrderID
-    , ProductID
-    , UnitePrice
-    , Quantity
-    , Discount
-    , (UnitePrice * Quantity) - (UnitePrice * Quantity * Discount) as Parcial
+        OrderID
+        , ProductID
+        , UnitePrice
+        , Quantity
+        , Discount
+        , (UnitePrice * Quantity) - (UnitePrice * Quantity * Discount) as Parcial
     FROM [Order Details]
 
-    -- Función Año en consulta
+-- Función Año en consulta
     SELECT
-    CustomerID
-    , OrderID  
-    , OrderDate
-    , YEAR(OrderDate) as Anio
+        CustomerID
+        , OrderID  
+        , OrderDate
+        , YEAR(OrderDate) as Anio
     FROM [Order Details]
 
-    -- Distinct: Para sacar duplicados
+-- Distinct: Para sacar duplicados
     SELECT DISTINCT 
-    Country
+        Country
     FROM Customers
-    Order by Country
+    ORDER BY Country
 
-    -- Función Case
+-- Función Case
     SELECT 
-    ProductName
-    , Category = 
-        CASE CategoryID
-            WHEN 1 THEN 'Bebidas'
-            WHEN 2 THEN 'Lacteos'
-            WHEN 3 THEN 'OTros'
-            ELSE 'No se encuentra en venta'
-        END -- AS Category
+        ProductName
+        , Category = 
+            CASE CategoryID
+                WHEN 1 THEN 'Bebidas'
+                WHEN 2 THEN 'Lacteos'
+                WHEN 3 THEN 'OTros'
+                ELSE 'No se encuentra en venta'
+            END -- AS Category
     FROM Products
     ORDER BY ProductName
 
-    -- COUNT: Devuelve el número de filas que coinciden con la condición especifica de la consulta. 
+-- COUNT: Devuelve el número de filas que coinciden con la condición especifica de la consulta. 
     
     SELECT count(first_name) FROM customers
 	
@@ -71,47 +81,73 @@
     FROM Film
     Group by rating -- Se usa el group by para agrupar por el conteo
 
-    -- JOINS (Para unir las tablas deben contener un campo en común)
+-- WHERE: Condiciones
+    SELECT 
+        rating as Rating
+        , count(*) as Num
+    FROM film
+    WHERE rating = 'R' or rating = 'NC-17'
+    GROUP BY rating
 
-    -- Inner Join (Uniones internas) 
-        -- Los datos en común 
-        /*
-            Tablas a utilizar para ver campo en común:
-                SELECT TOP 10 FROM Customers
-                SELECT TOP 10 FROM Orders
-        */
+-- ORDER BY: ASC DESC
+    SELECT 
+        store_id
+        , first_name
+        , last_name
+    FROM customer
+    ORDER BY store_id DESC, first_name ASC
 
-        -- Entre dos tablas
+-- LIMIT (TOP 10 - LIMIT 10)
+
+    SELECT first_name 
+    FROM customer
+    ORDER BY first_name ASC
+    LIMIT 10
+
+    SELECT TOP 10 first_name 
+    FROM customer
+
+-- JOINS (Para unir las tablas deben contener un campo en común)
+
+-- Inner Join (Uniones internas) Los datos en común.
+         
+                /*
+                    Tablas a utilizar para ver campo en común:
+                        SELECT TOP 10 FROM Customers
+                        SELECT TOP 10 FROM Orders
+                */
+
+    -- Entre dos tablas
         SELECT 
-        C.CustomerID
-        , C.Companyname
-        , C.[Address]
-        , C.ContactName
-        , C.City
-        , O.OrderID
-        , O.OrderDate
-        , O.ShippedDate
-        , O.ShippAddress
+            C.CustomerID
+            , C.Companyname
+            , C.[Address]
+            , C.ContactName
+            , C.City
+            , O.OrderID
+            , O.OrderDate
+            , O.ShippedDate
+            , O.ShippAddress
         FROM Customers AS C
         INNER JOIN Orders AS O
         ON C.CustomerID = O.CustomerID
 
-        -- Entre más tablas
-          SELECT 
-        C.CustomerID
-        , C.Companyname
-        , C.[Address]
-        , C.ContactName
-        , C.City
-        , O.OrderID
-        , O.OrderDate
-        , O.ShippedDate
-        , O.ShippAddress
-        , D.OrderID
-        , D.Quantity
-        , D.UnitePrice
-        , P.ProductID
-        , P.ProductName
+    -- Entre más tablas
+        SELECT 
+            C.CustomerID
+            , C.Companyname
+            , C.[Address]
+            , C.ContactName
+            , C.City
+            , O.OrderID
+            , O.OrderDate
+            , O.ShippedDate
+            , O.ShippAddress
+            , D.OrderID
+            , D.Quantity
+            , D.UnitePrice
+            , P.ProductID
+            , P.ProductName
 
         FROM Customers AS C
 
@@ -124,8 +160,8 @@
         INNER JOIN Products AS P 
         ON D.ProductID = P.ProductID
 
-    -- Outer Join (Uniones externas)
+-- Outer Join (Uniones externas)
 
-    -- Cross Join (Uniones cruzadas)
+-- Cross Join (Uniones cruzadas)
 
-    -- Selft Join (Union de tablas con sigo mismas)
+-- Selft Join (Union de tablas con sigo mismas)
